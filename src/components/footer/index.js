7181efd7a48efbfa8de58b98e8475a253449a6ef
Ritik Raj <ritikraj2629@gmail.com>
2022-06-13 10:29:27 +0530

    Initialize project using Create React App

---
+++ src/components/footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return <footer>Made with 🔥 by Raj</footer>;
+};
+
+export default Footer;
---
+++ src/components/header/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header">
+        <div className="header-logo">
+          Xpensr
+          <i className="fi-rr-credit-card" />.
+        </div>
+        <div className="header-button">
+          <a
+            href="https://github.com/shuvamk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="devicon-github-original colored" />
+            Star
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
